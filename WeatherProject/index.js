41350d2096abc98154ad8084a04565cb7610ced6
Binita-subedi <subedibinita94@gmail.com>
2021-05-21 15:24:15 +0545

    Get(all)method

--- WeatherProject/index.js
+++ WeatherProject/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const https = require('https');
 const app = express();
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({extended:true})); 
 const apikey ="0fb71efa3fd11fe4bb12a838666c5520";
 const url ="https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid="+apikey+"&units=metric";
 
@@ -9,24 +11,35 @@ app.listen(3000,function(){
 });
 
 app.get("/",function(request,response){
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(request,response)
+{
+  var cityName = request.body.cityname;
+  const url ="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apikey+"&units=metric";
   https.get(url,function(res){
   res.on("data",function(data){
-    var weatherinfo=JSON.parse(data);
-    var weather = weatherinfo.weather[0].main;
-    var temperature = weatherinfo.main.temp;
-    var icon = weatherinfo.weather[0].icon;
-    var imgURL = "http://openweathermap.org/img/wn/"+ icon+ "@2x.png"
-    response.write("<h1>The weather in kathmandu  = "+ weather);
-    response.write("<br>The temperature in Kathmandu = " + temperature + " degree Celcius");
-    response.write("<img src"+imgURL+">");
-    response.send();
+    
+    
+  const apikey ="0fb71efa3fd11fe4bb12a838666c5520";
+  var weatherinfo=JSON.parse(data);
+  var weather = weatherinfo.weather[0].main;
+  var temperature = weatherinfo.main.temp;
+  var place = weatherinfo.name;
+  var icon = weatherinfo.weather[0].icon;
+  var imgURL = "http://openweathermap.org/img/wn/"+ icon+ "@2x.png"
+  response.write("<h1>The weather in "+ place +"= "+ weather);
+  response.write("<br>The temperature = " + temperature + " degree Celcius");
+  response.write("<img src"+imgURL+">");
+  response.send();
   
 
 
 });
 });
 
-
-
+  
 });
+
 
